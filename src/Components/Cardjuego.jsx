4f854c0cd8b3098e7c6a53935177e2c98d8ef376
AndresGonzalez36 <misael.gonzalez1512@gmail.com>
2023-04-02 13:23:19 -0400

    Favoritos Solucionado

--- src/Components/Cardjuego.jsx
+++ src/Components/Cardjuego.jsx
@@ -3,36 +3,36 @@ import { Card, Button } from 'react-bootstrap';
 import Heart from './Heart.jsx';
 import ContextoGlobal from '../Context/ContextoGlobal.jsx';
 
-const Cardjuego = ({ fav, juegos }) => {
-  const { juego, setJuegos } = useContext(ContextoGlobal);
+const Cardjuego = ({ fav, juego }) => {
+  const { juegos, setJuegos } = useContext(ContextoGlobal);
 
   const eligeFavorito = (id) => {
-    const juegoAprov = juego.findIndex((j) => j.id === id);
-    juego[juegoAprov].liked = !juego[juegoAprov].liked;
-    setJuegos([...juego]);
+    const juegoAprov = juegos.findIndex((j) => j.id === id);
+    juegos[juegoAprov].liked = !juegos[juegoAprov].liked;
+    setJuegos([...juegos]);
   }
 
   return (
     <div>
       <Card style={{ width: '18rem', marginTop: '2em', marginLeft: '2em' }}>
-        <Card.Img variant="top" src={juegos.img} />
-        {!fav && <Button className="btnFav" onClick={() => eligeFavorito(juegos.id)} >
-         <Heart filled={juegos.liked}></Heart>
+        <Card.Img variant="top" src={juego.img} />
+        {!fav && <Button className="btnFav" onClick={() => eligeFavorito(juego.id)} >
+         <Heart filled={juego.liked}></Heart>
         </Button>
-        }console.log(juegos.id)
+        }console.log(juego.id)
         <Card.Body>
-          <Card.Title>{juegos.name}</Card.Title>
+          <Card.Title>{juego.name}</Card.Title>
           <hr></hr>
           <div>
             <h5>Disponibilidad</h5>
             <ul>
               {
-                juegos.dispo.map((i) => <li key={i} style={{ listStyleType: "none", textAlign: 'start' }} > {i}</li>)
+                juego.dispo.map((i) => <li key={i} style={{ listStyleType: "none", textAlign: 'start' }} > {i}</li>)
               }
             </ul>
           </div>
           <div>
-            <h4>${juegos.price}</h4>
+            <h4>${juego.price}</h4>
           </div>
           <Button>Ver más</Button>🎮
           <Button>Añadir</Button>
